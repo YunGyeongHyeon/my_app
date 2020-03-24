1182486867ec2efbf167ef1694a349656318790d
YunGyeongHyeon <y300513@gmail.com>
2020-03-24 17:13:49 +0900

    Updates

---
+++ precache-manifest.2f1ce8913490bd04b8e3ff8349f7339e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bb940eed0f90a6bfab77d648573bf21",
+    "url": "/my_app/index.html"
+  },
+  {
+    "revision": "1f8222302be5b7f5c8ef",
+    "url": "/my_app/static/css/main.26120bd7.chunk.css"
+  },
+  {
+    "revision": "8632ee97be0327bd4e97",
+    "url": "/my_app/static/js/2.83ec12de.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my_app/static/js/2.83ec12de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f8222302be5b7f5c8ef",
+    "url": "/my_app/static/js/main.7359ae60.chunk.js"
+  },
+  {
+    "revision": "d23cdb388744a34444d9",
+    "url": "/my_app/static/js/runtime-main.bde9112b.js"
+  }
+]);
